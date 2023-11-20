33547e0267b8f9493ebb6aef5c8c1bfe7bc58073
DarnellBT <titredarnb@gmail.com>
2023-11-20 04:36:44 +0000

    working grid

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from components import components
 
 app = Flask(__name__)
 
 @app.route('/')
-def main() -> str:
-    '''The main function injects elements for main.html. '''
-    return render_template('main.html')
+def index():
+    """The main function injects elements for main.html."""
+    return render_template('main.html', player_board=components.initialise_board())
 
 if __name__ == '__main__':
     app.run()
