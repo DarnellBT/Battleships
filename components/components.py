4234a838b723e52a006dca9a021dc1edd785eabd
DarnellBT <titredarnb@gmail.com>
2023-11-20 16:32:24 +0000

    Simple implementation for place_battleships

--- components/components.py
+++ components/components.py
@@ -4,22 +4,47 @@ initialise_board
 create_battleships
 place_battleships
 """
+from pathlib import Path
 
 # This is the type for board_state
-Board = list[list[str | None]]
+BoardState = list[list[str | None]]
 
-def initialise_board(size: int = 10) -> Board:
+def initialise_board(size: int = 10) -> BoardState:
     """Initialises board."""
-    board_state: Board = [[None for _ in range(size)] for _ in range(size)]
+    board_state: BoardState = []
+    row: list[None]
+    for _ in range(size):
+        row = []
+        for _ in range(size):
+            row.append(None)
+        board_state.append(row)
     return board_state
 
-def create_battleships(filename: str = 'battleships.txt') -> dict[str, int]:
+def create_battleships(filename: str = Path(__file__).parent.resolve() / 'battleships.txt') -> dict[str, int]:
     """Creates battleships."""
     with open(filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
-
-    battleships: dict[str, int] = {key: int(size) for key, size in line.split(',') for line in lines}
+    battleships: dict[str, int] = dict()
+    ship: str
+    ship_size: str
+    for line in lines:
+        # Each line in 'filename' has it's newline character removed if it is present and then split by the comma in each line where the left of the comma is assigned to ship and the right of the comma is assigned to ship_size.
+        ship, ship_size = line.replace('\n', '').split(',')
+        battleships[ship] = int(ship_size)
     return battleships
 
-def place_battleships():
-    """Places battleships."""
+def place_battleships(board_state: BoardState, ships: dict[str, int]) -> BoardState:
+    """Places battleships for the AI"""
+    new_board_state: BoardState = board_state
+    board_size: int = len(new_board_state)
+    for ship, ship_size in ships.items():
+        # This is for a horizontal placement for each ship from the left.
+        for row in range(board_size):
+            if any([new_board_state[row][ship_column] is not None for ship_column in range(ship_size)]):
+                continue
+            # Once a clear row to place ships is found the ship is placed and then the loop is broken out of.
+            for ship_column in range(ship_size):
+                new_board_state[row][ship_column] = ship
+            break
+    print(new_board_state)
+    return new_board_state
--- main.py
+++ main.py
@@ -8,10 +8,18 @@ from components import components
 
 app = Flask(__name__)
 
+BOARD_SIZE: int = 10
+ships: dict[str, int] = components.create_battleships()
+
 @app.route('/')
 def index():
     """The main function injects elements for main.html."""
-    return render_template('main.html', player_board=components.initialise_board())
+    return render_template('main.html', player_board=components.place_battleships(components.initialise_board(), ships))
+
+@app.route('/placement')
+def placement():
+    """The placement function injects placement elemenets for placement.html"""
+    return render_template('placement.html', board_size=BOARD_SIZE, ships=ships)
 
 if __name__ == '__main__':
     app.run()
