8bbf69731ee600bafd9173c2119d6cfeee4a4874
DarnellBT <titredarnb@gmail.com>
2023-11-20 05:25:54 +0000

    create_battleships

--- components/components.py
+++ components/components.py
@@ -6,15 +6,20 @@ place_battleships
 """
 
 # This is the type for board_state
-Board = list[list[dict[str, int] | None]]
+Board = list[list[str | None]]
 
 def initialise_board(size: int = 10) -> Board:
     """Initialises board."""
     board_state: Board = [[None for _ in range(size)] for _ in range(size)]
     return board_state
 
-def create_battleships():
+def create_battleships(filename: str = 'battleships.txt') -> dict[str, int]:
     """Creates battleships."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    battleships: dict[str, int] = {key: int(size) for key, size in line.split(',') for line in lines}
+    return battleships
 
 def place_battleships():
     """Places battleships."""
