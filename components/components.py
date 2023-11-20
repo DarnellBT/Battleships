3eddc648e1707f6079ad45002280d9a352048ab1
DarnellBT <titredarnb@gmail.com>
2023-11-20 04:12:41 +0000

    component update 1

--- components/components.py
+++ components/components.py
@@ -1,9 +1,21 @@
+"""
+Contains functions:
+initialise_board
+create_battleships
+place_battleships
+"""
 
+Board = list(list(dict[str, int] | None))
 
-def initialise_board():
+def initialise_board(size: int = 10) -> Board:
+    """Initialises board."""
+    board_state: Board = [[None for _ in range(size)] for _ in range(size)]
+    return board_state
 
 
 def create_battleships():
+    """Creates battleships."""
 
 
-def place_battleships(): 
+def place_battleships():
+    """Places battleships."""
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main() -> str:
     '''The main function injects elements for main.html. '''
-    return "Hello event driven world"
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
